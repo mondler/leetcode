8b24d037ed19f76f99684bb4210549d6f717bf0f
Yang Luo <mondler0418@yahoo.com>
2020-03-03 00:09:07 -0500

    Update 605

--- codes/605_Can_Place_Flowers.py
+++ codes/605_Can_Place_Flowers.py
@@ -1,3 +1,14 @@
+# https://leetcode.com/problems/can-place-flowers/description/
+# 605. Can Place Flowers
+# Easy
+#
+# 647
+#
+# 324
+#
+# Add to List
+#
+# Share
 # Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
 #
 # Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
@@ -18,6 +29,65 @@
 
 
 # %%
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return True
+        length = len(flowerbed)
+        cnt = 0
+        i = 0
+        while i < length:
+            if flowerbed[i] == 1:
+                i += 2  # if flower, savely jump to i + 2
+                continue
+            pre_clear = (i == 0) or (flowerbed[i - 1] == 0)
+            post_clear = (i == length - 1) or (flowerbed[i + 1] == 0)
+            if pre_clear and post_clear:
+                cnt += 1
+                if cnt == n:
+                    return True
+                # label flower so later spots can consider this
+                flowerbed[i] = 1
+                i += 2  # if flower, savely jump to i + 2
+                continue
+            i += 1  # if adjacent is flower, move to next
+        return False
+
+
+# %%
+flowerbed = [1, 0, 0, 0, 0, 0, 1]
+n = 2
+Solution().canPlaceFlowers(flowerbed, n)
+
+# %%
+
+
+class Solution:
+    def canPlaceFlowers(self, f, n):
+        L, i, c, f = len(f) - 2, -2, 0, f + [0]
+        while i < L:
+            i += 2
+            if f[i] == 1:
+                continue
+            if f[i + 1] == 0:
+                c += 1
+            else:
+                i += 1
+        return n <= c
+
+
+# %%
+f = [1, 0, 1, 0, 1]
+Solution().canPlaceFlowers(f, 1)
+
+# %%
+
+
 class Solution(object):
     def canPlaceFlowers(self, flowerbed, n):
         """
