9250be370a62676eaa1b39f2a487ede360ead129
Yang Luo <mondler0418@yahoo.com>
2021-05-08 00:13:47 -0500

    add #027 for c++

---
+++ codes_cpp/027_Remove_Element.cpp
@@ -0,0 +1,96 @@
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+//
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+//
+// The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+//
+// Example 1:
+//
+// Given nums = [3,2,2,3], val = 3,
+//
+// Your function should return length = 2, with the first two elements of nums being 2.
+//
+// It doesn't matter what you leave beyond the returned length.
+// Example 2:
+//
+// Given nums = [0,1,2,2,3,0,4,2], val = 2,
+//
+// Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+//
+// Note that the order of those five elements can be arbitrary.
+//
+// It doesn't matter what values are set beyond the returned length.
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+//
+
+class Solution {
+public:
+
+void swap(int& x, int&y){
+        int temp;
+        temp = x;
+        x = y;
+        y = temp;
+        return;
+}
+
+int removeElement(vector<int>& nums, int val) {
+        int i, j;
+        i = 0;
+        j = nums.size() - 1;
+
+        if (j < 0) {
+                return 0;
+        }
+
+        while ((j >= 0) && (nums[j] == val)) {
+                j--;
+        }
+
+        if (j <= 0) {
+                return (j + 1);
+        }
+
+        while (i <= j) {
+                // cout << i << ' ' << j << endl;
+                if (nums[i] != val) {
+                        i++;
+                }
+                else {
+                        swap(nums[i], nums[j]);
+                        i++;
+                        do  {
+                                j--;
+                        }
+                        while ((j >= i) && (nums[j] == val));
+                }
+                // for (const auto k: nums)
+                //         cout << k << ' ';
+                // cout << endl;
+                // cout << i << ' ' << j << endl;
+        }
+
+        return i;
+}
+};
+
+
+//
+
+int main() {
+        std::vector<int> nums = { 2,2,3};
+        int val = 2;
+
+        Solution foo = Solution();
+        cout << foo.removeElement(nums, val) << endl;
+
+        for (const auto i: nums)
+                cout << i << ' ';
+
+        return 0;
+}
