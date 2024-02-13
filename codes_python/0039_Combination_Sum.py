8025277b4f594d3b1c427b90625a46263d2563b5
Yang Luo <mondler0418@yahoo.com>
2024-02-12 23:24:26 -0600

    add files

--- codes_python/0039_Combination_Sum.py
+++ codes_python/0039_Combination_Sum.py
@@ -71,16 +71,17 @@ class Solution:
             if candidates[i] > target:
                 return
             cur.append(candidates[i])
-            print(cur)
+            # print(f"{cur=}")
+            # print(f"{res=}")
             self.dfs(candidates, target - candidates[i], i, cur, res)
             cur.pop()
         return
 
 
-# candidates = [2, 3, 6, 7]
-# target = 7
+candidates = [2, 3, 6, 7]
+target = 7
 
-candidates = [10, 1, 2, 7, 6, 1, 5]
-target = 8
+# candidates = [10, 1, 2, 7, 6, 1, 5]
+# target = 8
 
 print(Solution().combinationSum(candidates, target))
